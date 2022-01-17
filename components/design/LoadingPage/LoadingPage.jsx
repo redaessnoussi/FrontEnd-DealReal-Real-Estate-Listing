4454ab5df5ab753b8f8e66ac0f47ab1cb1900306
wolf21995 <71893153+wolf21995@users.noreply.github.com>
2022-01-17 14:18:41 +0100

    loader update

--- components/design/LoadingPage/LoadingPage.jsx
+++ components/design/LoadingPage/LoadingPage.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { css } from "@emotion/react";
-import { HashLoader } from "react-spinners";
+import { PropagateLoader } from "react-spinners";
 
 // Can be a string as well. Need to ensure each key-value pair ends with ;
 
 function LoadingPage() {
   return (
     <div className="w-full flex flex-col justify-center items-center h-full absolute top-0 bg-white">
-      <HashLoader size={150} color={`#27AD77`} />
+      <PropagateLoader size={150} color={`#27AD77`} />
     </div>
   );
 }
