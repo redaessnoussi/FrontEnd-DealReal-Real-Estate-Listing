5492a98b6b7182358b863686f3cf0d6bdbef3033
wolf21995 <71893153+wolf21995@users.noreply.github.com>
2022-03-03 17:10:36 +0100

    Update auth.js

--- pages/auth.js
+++ pages/auth.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.scss";
+// import Head from "next/head";
+// import Image from "next/image";
+// import styles from "../styles/Home.module.scss";
 import firebaseProvider from "../firebase/firebase";
 // Firebase Auth
 import {
