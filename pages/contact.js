6d570715e9c7714e9f1537eeb77801c0cd98949b
wolf21995 <71893153+wolf21995@users.noreply.github.com>
2022-04-22 05:25:45 +0000

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
+
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <>
+      <Head>
+        <title>Deal Real - Contact</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="container mx-auto p-7 h-96">
+        <h1>Comming Soon!</h1>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
