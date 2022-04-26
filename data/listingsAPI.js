0f3f367f48381088b1605e76b84796dc7e0ec582
wolf21995 <71893153+wolf21995@users.noreply.github.com>
2022-04-26 00:48:33 +0000

    Update listingsAPI.js

--- data/listingsAPI.js
+++ data/listingsAPI.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // Villas -> 3
 // Apartment -> 4
 // Office -> 5
@@ -8,27 +10,15 @@
 // Commercial Building -> 10
 // Other Commercial -> 11
 
-const hitsPerPage = 0;
-const listingCategory = 0;
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
-const listingsAPI = {
-  method: "GET",
-  url: "https://bayut.p.rapidapi.com/properties/list",
-  params: {
-    locationExternalIDs: "5002,6020",
-    purpose: "for-sale",
-    hitsPerPage: hitsPerPage,
-    page: "0",
-    lang: "en",
-    rentFrequency: "monthly",
-    categoryExternalID: listingCategory,
-  },
-  headers: {
-    "x-rapidapi-host": "bayut.p.rapidapi.com",
-    "x-rapidapi-key": "43ac89b881mshcc7664094152785p160748jsnce9c86269162",
-  },
+export const listingsAPI = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "43ac89b881mshcc7664094152785p160748jsnce9c86269162",
+    },
+  });
+
+  return data;
 };
-
-// console.log(listforSale.params.hitsPerPage);
-
-export default listingsAPI;
