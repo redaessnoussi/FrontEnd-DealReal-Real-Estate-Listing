015cd1bbbf5ad061784dfb5e33cc9e51e60b5c48
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-09-01 19:53:32 +0100

    Update ButtonLg.jsx

--- components/design/Buttons/ButtonLg.jsx
+++ components/design/Buttons/ButtonLg.jsx
@@ -1,8 +1,9 @@
-function ButtonLg({ className, children, type }) {
+function ButtonLg({ className, children, type, onClick }) {
   return (
     <button
       className={`flex items-center border-2 px-7 rounded-lg h-12 ${className}`}
       type={type}
+      onClick={onClick}
     >
       {children}
     </button>
