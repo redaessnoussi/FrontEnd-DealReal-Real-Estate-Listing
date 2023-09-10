de848a7ae1250c257c232ecb730295b9c600ff84
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-09-10 04:27:21 +0100

    firebase storage config

---
+++ firebase/storage.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPiVcLmUQKjWXj65GyzPHw9g6lsknQYvU",
+  authDomain: "deal-real.firebaseapp.com",
+  projectId: "deal-real",
+  storageBucket: "deal-real.appspot.com",
+  messagingSenderId: "113035359288",
+  appId: "1:113035359288:web:76a99f45ca93950000e2cd",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+export { storage };
