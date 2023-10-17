4a3cb96d22332f56bd86381d65bbec5b2a2e8d8f
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-10-17 16:19:51 +0100

    listing details: updated UI

--- pages/explore/[id]/index.js
+++ pages/explore/[id]/index.js
@@ -58,7 +58,7 @@ export default function ListingDetails({ listingDetail, properties }) {
           </p>
         </div>
         {/* multiple listing image */}
-        <div className={`${style.row} justify-between gap-y-4 mb-3`}>
+        <div className={`${style.row} justify-between gap-y-4 mb-8`}>
           <Image
             src={listingDetail[0].images[0].url}
             width={296}
@@ -92,58 +92,78 @@ export default function ListingDetails({ listingDetail, properties }) {
             className="rounded-lg object-cover"
           />
         </div>
-        {/* listing description and details */}
-        <h4>Description</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        {/* lisiting overview */}
-        <h4>Overview</h4>
-        <div className={`${style.row} justify-between gap-y-4 mb-3`}>
-          <p>4 Bedrooms</p>
-          <p>3 Bathrooms</p>
-          <p>1 Garage</p>
-          <p>3,000 sqft</p>
+        <div className={`${style.row} justify-between`}>
+          <div className="w-7/12">
+            {/* listing description and details */}
+            <h4 className="text-title-800 font-bold">Description</h4>
+            <p className="mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            {/* lisiting overview */}
+            <h4 className="text-title-800 font-bold">Overview</h4>
+            <div className={`${style.row} justify-between gap-y-4 mb-8`}>
+              <p className="text-title-800">4 Bedrooms</p>
+              <p className="text-title-800">3 Bathrooms</p>
+              <p className="text-title-800">1 Garage</p>
+              <p className="text-title-800">3,000 sqft</p>
+            </div>
+            {/* listing details */}
+            <h4 className="text-title-800 font-bold">Details</h4>
+            <div className={`${style.row} justify-between mb-8`}>
+              <div className="w-6/12">
+                <h5 className="text-title-800 font-bold">
+                  Property ID{" "}
+                  <span className="text-body-800 font-normal">AGS1234</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Price{" "}
+                  <span className="text-body-800 font-normal">$ 250,000</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Property Size{" "}
+                  <span className="text-body-800 font-normal">3000 sqft</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Bedroom <span className="text-body-800 font-normal">4</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Bathroom <span className="text-body-800 font-normal">3</span>
+                </h5>
+              </div>
+              <div className="w-6/12">
+                <h5 className="text-title-800 font-bold">
+                  Garage <span className="text-body-800 font-normal">1</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Garage Size{" "}
+                  <span className="text-body-800 font-normal">200 sqft</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Year Built{" "}
+                  <span className="text-body-800 font-normal">2001</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Category{" "}
+                  <span className="text-body-800 font-normal">House</span>
+                </h5>
+                <h5 className="text-title-800 font-bold">
+                  Property Status{" "}
+                  <span className="text-body-800 font-normal">Sale</span>
+                </h5>
+              </div>
+            </div>
+            <h4 className="font-bold text-slate-700">Address</h4>
+          </div>
+          <div className="w-5/12">
+            <h3>Reservation Card</h3>
+          </div>
         </div>
-        {/* listing details */}
-        <h4>Overview</h4>
-        <h5>
-          Property ID <span>AGS1234</span>
-        </h5>
-        <h5>
-          Price <span>$ 250,000</span>
-        </h5>
-        <h5>
-          Property Size <span>3000 sqft</span>
-        </h5>
-        <h5>
-          Bedroom <span>4</span>
-        </h5>
-        <h5>
-          Bathroom <span>3</span>
-        </h5>
-        <h5>
-          Garage <span>1</span>
-        </h5>
-        <h5>
-          Garage Size <span>200 sqft</span>
-        </h5>
-        <h5>
-          Year Built <span>2001</span>
-        </h5>
-        <h5>
-          Category <span>House</span>
-        </h5>
-        <h5>
-          Property Status <span>Sale</span>
-        </h5>
-        <h4>Address</h4>
       </div>
       {/* listing address on the map */}
       <div className="bg-green-200 h-96 relative flex justify-center">
@@ -151,8 +171,8 @@ export default function ListingDetails({ listingDetail, properties }) {
       </div>
       {/* contact information */}
       <div className="container mx-auto px-7 py-7">
-        <h4>Contact Information</h4>
-        <h5>Grand House Real Estate</h5>
+        <h4 className="text-title-800 font-bold">Contact Information</h4>
+        <h5 className="text-title-800 font-bold">Grand House Real Estate</h5>
         <p>+90 8989787 898</p>
         <p>+90 8989787 898</p>
         <ButtonMd
