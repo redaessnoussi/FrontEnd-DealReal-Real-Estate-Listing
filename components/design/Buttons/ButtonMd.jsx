02735bd9f41dc6880036f1ec1c263bb7314f3f76
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-10-17 14:27:35 +0100

    added on click props on button medium size

--- components/design/Buttons/ButtonMd.jsx
+++ components/design/Buttons/ButtonMd.jsx
@@ -1,6 +1,10 @@
-function ButtonMd({ className, children, type }) {
+function ButtonMd({ className, children, type, onClick }) {
   return (
-    <button className={`px-8 h-9 border-2 rounded-lg ${className}`} type={type}>
+    <button
+      className={`px-8 h-9 border-2 rounded-lg ${className}`}
+      type={type}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
