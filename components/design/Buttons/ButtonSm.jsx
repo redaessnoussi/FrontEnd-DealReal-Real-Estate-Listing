20660046293413564da2ffebcda370289efeb2e5
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-10-21 17:16:43 +0100

    new param for small btn

--- components/design/Buttons/ButtonSm.jsx
+++ components/design/Buttons/ButtonSm.jsx
@@ -1,9 +1,18 @@
-function ButtonSm({ className, children, type }) {
+function ButtonSm({ className, children, type, roundedBtn = false }) {
   return (
-    <button className={`px-4 h-9 border-2 rounded-lg ${className}`} type={type}>
+    <button
+      className={
+        roundedBtn
+          ? `w-10 h-10 flex justify-center items-center border-2 rounded-full ${className}`
+          : `px-4 h-9 border-2 rounded-lg ${className}`
+      }
+      type={type}
+    >
       {children}
     </button>
   );
 }
 
 export default ButtonSm;
+
+//w-10 h-10 flex justify-center items-center border-2 rounded-full ${className}
