36b7f9a1335adf1e386b261dd7af92e8d18cd94a
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-11-16 15:38:24 +0100

    backend: login and register users with modal

--- backend/schema/users.models.js
+++ backend/schema/users.models.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  fullName: { type: String, required: true },
   role: { type: String, enum: ["client", "agent"], required: true },
   agencyName: { type: String }, // Only applicable to agents
   avatar: [
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
       url: String, // Actual image URL
     },
   ],
+  createdAt: {
+    type: Date,
+    default: Date.now, // Add a default value of the current date and time
+  },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Users", userSchema);
