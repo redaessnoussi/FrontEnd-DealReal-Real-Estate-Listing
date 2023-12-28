946fce4a1bf3271676c6a2f497248a93c35aacdb
wolf21995 <71893153+redaessnoussi@users.noreply.github.com>
2023-12-28 17:36:11 +0100

    feat: added modal to show listing image with bigger size

--- components/ImageModal/ImageModal.jsx
+++ components/ImageModal/ImageModal.jsx
@@ -1,16 +1,45 @@
+// import ButtonLg from "components/design/Buttons/ButtonLg";
 import Image from "next/image";
 import React from "react";
 import Modal from "react-modal";
 
 const ImageModal = ({ isOpen, onRequestClose, selectedImage }) => {
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Image Modal"
-    >
-      <Image src={selectedImage} layout="fill" alt="Selected Property" />
-    </Modal>
+    <div className="relative">
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Image Modal"
+        style={{
+          overlay: {
+            height: "100%",
+            position: "fixed",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#2229",
+          },
+          content: {
+            position: "relative",
+            inset: "0",
+            width: "80%",
+            maxHeight: "90vh",
+            padding: "0",
+            border: "0",
+          },
+        }}
+      >
+        <Image
+          src={selectedImage}
+          layout="responsive"
+          width={822}
+          height={522}
+          alt="Selected Property"
+          objectFit="cover"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </Modal>
+    </div>
   );
 };
 
